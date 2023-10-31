fe2ab3fa5c8448defc51971560d045e4ef266f11
Jan Kowalski <18rakso18@gmail.com>
2023-10-31 09:47:47 +0100

    Initial commit

---
+++ src/Dane.java
@@ -0,0 +1,12 @@
+public class Dane {
+    public static void main(String[] args) {
+        //31.10.2023 9:42
+      string name;
+
+        System.out.println();
+    }
+
+
+
+
+}
---
+++ src/NajlepszeGry.java
@@ -0,0 +1,17 @@
+public class NajlepszeGry {
+    public static void main(String[] args) {
+        System.out.println("Według mnie najlepsze gry to:");
+        System.out.println("Minecraft");
+        System.out.println ("WOT ");
+        System.out.println("CS 1.6");
+        System.out.println("Dying Light");
+        System.out.println("Wedlug mnie najgorsze gry to:");
+        System.out.println("War Thunder");
+        System.out.println("Counter Strike 2 ");
+        System.out.println("Dota");
+
+
+    }
+
+}
+
